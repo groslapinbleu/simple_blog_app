e7ccf0aec9a5ab715859610d444a035063d3f74e
groslapinbleu <antoine.jouannais@gmail.com>
2021-08-05 22:25:37 +0200

    Initial commit

---
+++ src/screens/IndexScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default IndexScreen;
