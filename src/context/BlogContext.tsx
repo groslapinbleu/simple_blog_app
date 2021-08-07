17941430f3d1bb55cb29b92106f21eef7254fe83
groslapinbleu <antoine.jouannais@gmail.com>
2021-08-07 19:58:08 +0200

    136: refactoring, replaced useState with useReducer

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -1,35 +1,45 @@
-import React, { useState } from 'react';
-export interface BlogPostInterface {
+import React, { useReducer } from 'react';
+interface BlogPostInterface {
   title: string;
   content: string;
 }
 
-const sampleBlogPost1: BlogPostInterface = {
-  title: 'Example blog post',
-  content: 'Lorem Ipsum',
-};
+interface ActionInterface {
+  type: string;
+}
 
-const sampleBlogPost2: BlogPostInterface = {
-  title: 'Another example blog post',
-  content: 'Lorem Ipsum tada',
-};
-
-const defaultBlog = [sampleBlogPost1, sampleBlogPost2];
+const defaultBlogPosts: BlogPostInterface[] = [
+  //   {
+  //     title: 'Example blog post',
+  //     content: 'Lorem Ipsum',
+  //   },
+  //   {
+  //     title: 'Another example blog post',
+  //     content: 'Lorem Ipsum tada',
+  //   },
+];
 
 const BlogContext = React.createContext({
-  data: defaultBlog,
+  data: defaultBlogPosts,
   addBlogPost: () => {},
 });
 
+function blogReducer(blogPosts: BlogPostInterface[], action: ActionInterface) {
+  switch (action.type) {
+    case 'add_blogPost':
+      return [
+        ...blogPosts,
+        { title: `Blog post #${blogPosts.length + 1}`, content: 'Any content' },
+      ];
+    default:
+      throw new Error();
+  }
+}
 export const BlogProvider = ({ children }: any) => {
-  const [blogPosts, setBlogPosts] = useState(defaultBlog);
+  const [blogPosts, dispatch] = useReducer(blogReducer, defaultBlogPosts);
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog post #${blogPosts.length + 1}`, content: 'Any content' },
-    ]);
+    dispatch({ type: 'add_blogPost' });
   };
-
   return (
     <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
